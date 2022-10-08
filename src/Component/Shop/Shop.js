bdac9fb21b294e8b0c725c4db2eed20d1e3cfecf
ataulweb <ataulweb@gmail.com>
2022-10-08 06:21:59 +0600

    added shop section

---
+++ src/Component/Shop/Shop.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Shop.css'
+
+const Shop = () => {
+    return (
+        <div className='shop-container'>
+            <div className="product-container">
+                <h2>This is for Products</h2>
+            </div>
+
+            <div className="cart-container">
+                <h3>Order Summary</h3>
+            </div>
+            
+        </div>
+    );
+};
+
+export default Shop;
