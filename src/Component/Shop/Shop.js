da52bf22fa31ad912754c66b4f2767a4117396b4
ataulweb <ataulweb@gmail.com>
2022-10-08 07:31:14 +0600

    added product component

--- src/Component/Shop/Shop.js
+++ src/Component/Shop/Shop.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Products from '../Product/Product';
 import './Shop.css'
 
 const Shop = () => {
+    const [products, setProducts]= useState([]);
+
+    useEffect( () =>{
+        fetch('products.json')
+        .then (res =>res.json())
+        .then (data => setProducts(data))
+    }, [])
     return (
         <div className='shop-container'>
             <div className="product-container">
-                <h2>This is for Products</h2>
+                {
+                    products.map(product => <Products
+                    key={product.id}></Products>)
+                }
             </div>
 
             <div className="cart-container">
